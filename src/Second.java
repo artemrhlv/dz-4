21c76b376af2575877af13bb1024a389a417c6eb
artemrhlv <osprey@ukr.net>
2023-04-01 21:12:37 +0300

    second commit

---
+++ src/Second.java
@@ -0,0 +1,10 @@
+public class Second {
+
+        public int a = 21;
+
+        public String print(){
+            String printsecond = String.format("---<%d>---", a);
+            System.out.println(printsecond);
+            return printsecond;
+    }
+}
